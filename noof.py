4cce94829d87173e2b4fa1eedafb166d5e1b37da
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-01-11 13:44:41 +0530

    Update noof.py

--- noof.py
+++ noof.py
@@ -1,8 +1,8 @@
 
 #jyothi
 count = 0
-number = int(input("Enter a number "))
+number = int(input(""))
 while (number > 0):
   number = number//10
   count = count + 1
-print ("Total number of digits : ",count)
+print (count)
