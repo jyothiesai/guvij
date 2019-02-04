bb43bd75695a94bb3c235374fa8bc909c4f06540
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-04 22:50:19 +0530

    Update nearest.py

--- nearest.py
+++ nearest.py
@@ -1,11 +1,10 @@
 #jyothi
-n=int(raw_input())
-m=n+1
+n=int(input(""))
 i=1
-while m!=0:
-    k=2**i
-    if m==k:
-        print m
-        break
-    i=i+1
-    m=m+1
+sum=1
+for i in range(1,n):
+ sum=sum*2;
+ if(sum==n):
+     sum=sum+n
+     print(sum) 
+i=i+1
