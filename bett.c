cc660dbdb7961b7ed2beb519bb8f00cf8c1fbe42
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-06 22:56:29 +0530

    Update and rename bett.py to bett.c

---
+++ bett.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a>b)
+	{
+		if(a<c)
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
--- bett.py
+++
@@ -1,7 +0,0 @@
-#jyothi
-n=raw_input()
-n1,n2=list(map(int,raw_input().split()))
-if n>n1 and n<n2:
-	print("yes")
-else:
-	print("no")
