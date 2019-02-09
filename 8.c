de962bc9f2975fc15848d4ae906fc65f3c79b832
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-09 20:49:50 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,17 @@
+//jyothi
+#include <stdio.h>
+int main()
+{
+int n,k,a[4],i;
+scanf("%d",&n);
+scanf("%d",&k);
+for(i=0;a[i]!='\0';i++)
+{
+scanf("%d",&a[i]);
+if(a[i]==k)
+{
+printf("%d",k);
+}
+}
+return 0;
+}
