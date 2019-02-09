2211e1fd50b80a13d19bba0d1caeee98b9032754
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-09 22:04:08 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -4,7 +4,7 @@ if x1 > y1:
     smaller = y1
 else:
     smaller = x1
-for i in range(0, smaller):
+for i in range(1, smaller+1):
     if((x1 % i == 0) and (y1 % i == 0)):
     	gcd1 = i
 print(gcd1)
