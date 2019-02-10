a29e16815204759a4ca35c70c0e2d549df5691be
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-10 21:17:39 +0530

    Create alter.py

---
+++ alter.py
@@ -0,0 +1,17 @@
+#jyothi
+n=raw_input()
+l1=[]
+l=raw_input().split()
+for x in l:
+    l1.append(int(x))
+x=1
+while (x<len(l1)):
+    if(x%2!=0 and l1[x]>l1[x-1]):
+        x=x+1
+    elif(x%2==0 and l1[x]<l1[x-1]):
+        x=x+1
+    else:
+        print("no")
+        break
+else:
+    print("yes")
