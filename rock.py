1ef18bc93dd2dcbdeb08ee8d3b7eee4e583c6319
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-10 21:48:05 +0530

    Create rock.py

---
+++ rock.py
@@ -0,0 +1,19 @@
+#jyothi
+P='paper'
+R='Rock'
+S='Scissor'
+user1,user2=raw_input('').split(" ")
+if(user1=='P' and user2=='R'):
+  print('P')
+elif(user1=='P' and user2=='S' ):
+  print('S')
+elif(user1=='R' and user2=='P' ):
+  print('P')
+elif(user1=='R' and user2=='S' ):
+  print('R')
+elif(user1=='S' and user2=='P' ):
+  print('S')
+elif(user1=='S' and user2=='R'):
+  print('R')
+else:
+  print('Invalid')
