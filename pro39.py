184ed2e5cf76a3947d91dff5405b0675e9a421f9
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-11 22:09:46 +0530

    Create pro39.py

---
+++ pro39.py
@@ -0,0 +1,15 @@
+#jyothi
+def Power(a):
+    if (a == 0):
+        return False
+    while (a != 1):
+            if (a % 2 != 0):
+                return False
+            a = a // 2
+             
+    return True
+n=int(input())
+if(Power(n)):
+    print('yes')
+else:
+    print('no')
