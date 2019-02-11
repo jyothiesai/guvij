8bf1a3037cef43fa646f3a88b25217a77eac3680
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-11 17:40:42 +0530

    Update sot.py

--- sot.py
+++ sot.py
@@ -1,14 +1,11 @@
 #jyothi
-p=int(input())
-l=list(1 for i in range(p))
-li=[]
-for i in range(2,p,1):
-    if l[i]==1:
-        for j in range(i*i,p,i):
-            l[j]=0
-        if p%i==0:
-            li.append(i)
-        else:
-        	print(p)
-        	break
-print(" ".join(str(x) for x in li))
+a=int(input())
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if i*j==a and i != 1:
+            d=0
+            for k in range(2,i):
+                if i%k==0:
+                    d=d+1
+            if d==0:
+                print(i,end=" ")
