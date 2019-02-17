3cab3478925fde30694400cbcb8f8095d025ad67
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-17 14:35:17 +0530

    Create small.py

---
+++ small.py
@@ -0,0 +1,13 @@
+#jyothi
+n,m=map(int,raw_input().split())
+l=raw_input().split()
+l1=[]
+s=[]
+for x in l:
+    l1.append(int(x))
+for x in l1:
+    if(x<m):
+        s.append(x)
+s.sort()
+k=" ".join(map(str,s))
+print(k)
