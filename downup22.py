69dcd23ba0afa5e87e5c556ec7328eacb888df52
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-18 17:15:36 +0530

    Update downup22.py

--- downup22.py
+++ downup22.py
@@ -1,4 +1,13 @@
 #jyothisridhar
-N=int(raw_input())
-a=(raw_input()).split()
-print(" ".join(max(a)))
+n=int(raw_input())
+l=list(map(int,raw_input().split()))
+c=1
+p=[]
+for i in range(0,len(l)-1):
+        if l[i]<l[i+1]:
+                c=c+1
+        else:
+               p.append(c)
+               c=1
+p.append(c)
+print(max(p))
