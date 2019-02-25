6a3e6abdbc0292734c0c804477552ca442825d75
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-02-25 17:39:28 +0530

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,12 @@
+#jyothi
+N=(raw_input())
+M=(raw_input())
+a=[]
+l=int(len(a))
+if(M in N):
+    a=N.replace(M,"")
+if(a[0]==" "):
+    a=a.replace(" ","")    
+for i in a:   
+    a=a.replace("  "," ")        
+print("".join(str(i) for i in a))
