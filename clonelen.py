4f236fc8c5675e29fac23de3e59e06c710ec9476
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-03-05 15:55:38 +0530

    Create clonelen.py

---
+++ clonelen.py
@@ -0,0 +1,13 @@
+#jyothi
+n=int(raw_input())
+d={}
+v=["a","e","i","o","u","A","E","I","O","U"]
+for i in range(n):
+  s=raw_input()
+  c=0
+  for i in s:
+    if i in v:
+      c+=1
+  d[s]=c
+for i in (sorted(d.items(), key = lambda kv:(kv[1], kv[0]),reverse=True)):
+  print(i[0])
