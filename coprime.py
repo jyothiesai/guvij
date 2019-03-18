54ed2b6900688cf28d1ff6f3dc316ca93afeb6ff
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-03-18 16:54:39 +0530

    Create coprime.py

---
+++ coprime.py
@@ -0,0 +1,13 @@
+#jyothi
+s1,s2=map(str,raw_input().split())
+x=len(s1)
+y=len(s2)
+n=max(x,y)
+c=0
+for i in range(1,n):
+    if x%i==0 and y%i==0:
+        c=c+1
+if c==1:
+    print("yes")
+else:
+    print("no")
