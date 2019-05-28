448d693c01adcebabc52acda4de9bea773c79bd9
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-05-28 17:02:06 +0530

    Update pickneigh.py

--- pickneigh.py
+++ pickneigh.py
@@ -3,6 +3,6 @@ n=int(raw_input())
 l=list(map(int,raw_input().split()))
 s=[]
 for i in range(0,len(l)):
-	for j in range(2,len(l)):
+	for j in range(2,len(l)+1):
 		s.append(sum(l[i:j:2]))
 print(max(s))
