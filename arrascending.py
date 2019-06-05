1262b29957c8b765f5a00347fcc30c4312c8680d
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-06-05 21:09:00 +0530

    Update arrascending.py

--- arrascending.py
+++ arrascending.py
@@ -1,6 +1,15 @@
 #jyothisridhar
-d=int(raw_input())
-e=list(map(str,raw_input().split()))
-j=sorted(e)
-c=j[::-1]
-print(" ".join(c))
+n=int(raw_input())
+a=(raw_input().split())
+a.sort()
+l=[]
+s=[]
+for i in a:
+    l.append(len(i))
+l.sort()
+for x in l:
+    for y in a:
+        if(len(y)==x):
+            if y not in s:
+                s.append(y)
+print(" ".join(s))
