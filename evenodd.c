b572e862d5d66447f793562725c7ef35f1dc6e7f
jyothi <42904507+jyothiesai@users.noreply.github.com>
2019-08-06 21:32:02 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<=n;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			printf("even");
+			break;
+		}
+		else{
+			printf("odd");
+			break;
+		}
+	}
+	return 0;
+}
